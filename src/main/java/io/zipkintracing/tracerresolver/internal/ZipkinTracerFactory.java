101d18f5a11029f644e71f9187f0888832f3abcc
Abhinav Sarje <abhinav@oneracommerce.com>
2019-07-29 17:44:06 -0700

    read in properties

--- src/main/java/io/zipkintracing/tracerresolver/internal/ZipkinTracerFactory.java
+++ src/main/java/io/zipkintracing/tracerresolver/internal/ZipkinTracerFactory.java
@@ -9,14 +9,38 @@ import io.opentracing.noop.NoopTracerFactory;
 import zipkin2.reporter.AsyncReporter;
 import zipkin2.reporter.urlconnection.URLConnectionSender;
 
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ZipkinTracerFactory implements TracerFactory {
 
+  public static final String[] PROPERTIES = {
+      "zipkin.endpoint",
+      "zipkin.servicename"
+  };
+
   private final static String ZIPKIN_URL = "https://zipkin-staging.onerainc.com/api/v2/spans";
 
+  private final static Logger logger = Logger.getLogger(ZipkinTracerFactory.class.getName());
+
   public Tracer getTracer() {
+
+    Properties props = ZipkinTracerParameters.loadConfigurationFile();
+    for (String paramName : PROPERTIES) {
+      String paramValue = System.getProperty(paramName);
+      if (paramValue != null) {
+        props.setProperty(paramName, paramValue);
+      }
+    }
+    for (String propName: props.stringPropertyNames()) {
+      logger.log(Level.INFO, "Retrieved Tracer parameter " + propName + "=" + props.getProperty(propName));
+    }
+
+    // ...
+
     try {
       final URLConnectionSender sender = URLConnectionSender.create(ZIPKIN_URL);
       final AsyncReporter reporter = AsyncReporter
---
+++ src/main/java/io/zipkintracing/tracerresolver/internal/ZipkinTracerParameters.java
@@ -0,0 +1,43 @@
+package io.zipkintracing.tracerresolver.internal;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ZipkinTracerParameters {
+
+  public static final String DEFAULT_CONFIGURATION_FILEPATH = "tracer.properties";
+  public static final String CONFIGURATION_FILE_KEY = "tracer.configFile";
+
+  private final static Logger logger = Logger.getLogger(ZipkinTracerParameters.class.getName());
+
+  // Private constructor to disable instantiation.
+  private ZipkinTracerParameters() { }
+
+  public static Properties loadConfigurationFile() {
+    String path = System.getProperty(CONFIGURATION_FILE_KEY);
+    if (path == null) {
+      path = DEFAULT_CONFIGURATION_FILEPATH;
+    }
+
+    Properties props = new Properties();
+    File configFile = new File(path);
+    if (!configFile.isFile()) {
+      return props;
+    }
+
+    try (FileInputStream stream = new FileInputStream(configFile)) {
+      props.load(stream);
+    } catch (IOException e) {
+      logger.log(Level.WARNING, "Failed to read Tracer configuration file " + path);
+      logger.log(Level.WARNING, e.toString())
+    }
+
+    logger.log(Level.INFO, "Successfully loaded Tracer configuration file " + path);
+    return props;
+  }
+
+}
